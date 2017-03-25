3af245bc508d16491f76660030bb5630ec8054f7
kdr35 <kdr35@pitt.edu>
2017-03-25 16:06:45 -0400

    test

--- src/main/java/edu/infsci2560/services/KMCatalogEntriesService.java
+++ src/main/java/edu/infsci2560/services/KMCatalogEntriesService.java
@@ -7,8 +7,6 @@ package edu.infsci2560.services;
 
 import edu.infsci2560.models.KMCatalogEntry;
 import edu.infsci2560.models.DocumentCategory;
-//import edu.infsci2560.models.KMCatalogEntry.DocumentCategory;
-import edu.infsci2560.models.KMCatalogEntry.Community;
 import edu.infsci2560.repositories.KMCatalogEntryRepository;
 import java.util.ArrayList;
 import java.util.HashMap;

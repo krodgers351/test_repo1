a9157bf315e706a4b473e7a8be62e1dd98700941
kdr35 <kdr35@pitt.edu>
2017-04-21 19:30:06 -0400

    add

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -17,6 +17,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		registry.addViewController("/communities").setViewName("communities");
 		registry.addViewController("/departments").setViewName("departments");
 		registry.addViewController("/d3tree").setViewName("d3tree");
+		registry.addViewController("/searchcatalog").setViewName("searchcatalog");
         registry.addViewController("/login").setViewName("login");
     }
 
